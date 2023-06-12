892837766827e7034d9dd2c6971e7ef001f9bbee
sam fallowfield <sam.fallowfield1@gmail.com>
2023-06-12 18:04:43 +0100

    Created users controller and basic Crud functionality

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+class UsersController < ApplicationController
+  before_action :set_user, except: [:create, :index]
+
+  def index
+    @users = User.all
+    render json:@users, status: :ok
+  end
+
+  def show
+    render json: @user, status: :ok
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: {errors: @user.errors.full_messages},
+             status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: {errors: @user.errors.full_messages}
+    end
+  end
+
+  def destroy
+    @user.destroy
+  end
+
+
+
+  private
+
+  def set_user
+    @user = User.find_by_username!(params[:username])
+  end
+
+  def user_params
+    params.permit( :username, :email, :password)
+  end
+end
